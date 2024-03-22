c5224f2b0baf16d5433ced0c9efa5b7767909fef
TheColdblood <simongregory05@gmail.com>
2024-03-22 11:52:19 +0100

    jq2

--- script.js
+++ script.js
@@ -4,40 +4,49 @@ window.addEventListener("load", function () {
     elemekElerese3();
     elemekElerese4();
     elemekFormazasa1();
+    esemenykezeles1();
 });
 
 function elemekElerese1() {
     /**Ide jön az első feladat */
-    const ELEM = document.querySelectorAll("section h2")[0];
-    ELEM.innerHTML;
-    console.log(ELEM);
+    const ELEM = $("#f1");
+    let kiir = ELEM.html();
+    console.log(kiir);
 
 }
 function elemekElerese2() {
     /**Ide jön az 2.  feladat */
-    const ELEM = document.getElementById("ide");
-    ELEM.innerHTML = "<p> Jó reggelt! </p>";
+    const ELEM = $("#ide");
+    ELEM.html("Jó reggelt!");
 }
 function elemekElerese3() {
     /**Ide jön az 3. feladat */
-    const ELEM = document.getElementsByClassName("ide")[0];
-    ELEM.innerHTML = "<p> Jó reggelt! </p>"
+    const ELEM = $(".ide");
+    ELEM.html ("<p> Jó reggelt! </p>");
 }
 
 function elemekElerese4() {
     /**Ide jön az 4. feladat */
-    const ELEM = document.getElementsByClassName("lista")[0];
+    const ELEM = $(".lista");
     let txt = "<ul>"
     for (let index = 0; index < 5; index++){
         randomSzam = Math.floor(Math.random() * 20) + 10;
         txt += `<li>${randomSzam}</li>`
     }
     txt +="</ul>"
-    ELEM.innerHTML = txt;
+    ELEM.html(txt);
 }
 
 function elemekFormazasa1() {
     /**Ide jön az 5. feladat */
-    const ELEM = document.getElementsByClassName("lista")[0];
-    ELEM.classList.add("formazott");
+    const ELEM = $(".lista");
+    ELEM.addClass("formazott");
+}
+
+function esemenykezeles1(){
+    const ELEM = $(".lista")
+    ELEM.on("click", function(){
+        const ELEM2 = $(".kattintasutan");
+        ELEM2.html(ELEM.html());
+    });
 }
